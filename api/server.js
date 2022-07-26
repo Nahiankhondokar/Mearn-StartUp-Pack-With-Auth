5da8ff03338fbfeaba303916153d3842af581f94
Nahiankhondokar <nahiankhondokarphp@gmail.com>
2022-07-26 18:14:50 +0600

    mearn start up project with auth express is ready

---
+++ api/server.js
@@ -0,0 +1,39 @@
+import dotenv from 'dotenv';
+import express from 'express';
+import colors from 'colors';
+import studentRoute from './routes/students.js';
+import userRoute from './routes/user.js';
+import mongoDBConnection from './config/db.js';
+import errorHandler from './middlewares/errorHandler.js';
+import cookieParser from 'cookie-parser';
+
+
+// express initialize
+const app = express();
+
+
+// environment setup
+dotenv.config();
+const PORT = process.env.SERVER_PORT || 5000;
+
+
+// Post data Middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended : false }));
+app.use(cookieParser());
+
+
+
+// rotues
+app.use('/api/students', studentRoute);
+app.use('/api/users', userRoute);
+
+// custom error handler middleware
+app.use(errorHandler);
+
+// server listen
+app.listen(PORT, () => {
+    // mongoDB connection
+    mongoDBConnection();
+    console.log(`Server is listening on port ${PORT}`.bgGreen.black);  
+});
